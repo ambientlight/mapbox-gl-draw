e3c610b288292c5375ac09ee94b784f524680b0c
ambientlight <primary.taras.vozniuk@gmail.com>
2017-05-15 23:58:51 +0800

    setupDraw -> MapboxGL.Control

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,8 @@
-declare namespace MapboxGLDraw {
-    function setupDraw(options?: any): any;
-}
-
-
 declare module '@mapbox/mapbox-gl-draw' {
+    import * as MapboxGL from 'mapbox-gl';
+    namespace MapboxGLDraw {
+        function setupDraw(options?: any): MapboxGL.Control;
+    }
+
 	export = MapboxGLDraw;
 }
